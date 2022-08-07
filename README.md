3106f01d60d2575a7249ec52d34c1c1b906452c0
Mik Mueller <83001409+MikMuellerDev@users.noreply.github.com>
2022-08-07 11:18:01 +0200

    Add setup-only env variable, write `README.md`

--- README.md
+++ README.md
@@ -11,13 +11,43 @@ make docker
 
 ```bash
 # Navigate into the `smarthome-go/smarthome` repository
-cd smarthome
+# cd smarthome
 
 # Run following command AFTER the image has been built
 # This will start the integration tests using a local version of the code
 docker run -it \
   --rm \
   --name smarthome-server-integration-tests \
+  -e 'SMARTHOME_INTEGRATION_SETUP_ONLY'=0 \
+  -v $(pwd):/opt/smarthome/tests \
+  mikmuellerdev/smarthome-integration-test
+```
+
+### Setup Only
+Sometimes it is desired to only setup MariaDB and check the current directory for validity, in those cases the
+`SMARTHOME_INTEGRATION_SETUP_ONLY1` environment variable can be set to `1`.
+
+Those cases include using the image to perform other tests on the server repository, for example running the actual server.
+
+#### Valid Values
+| Value | Result                                |
+|-------|---------------------------------------|
+| `0`   | Continuation of the integration tests |
+| `1`   | Abortion using exit-code `0`          |
+| empty | Continuation of the integration tests |
+| any   | Error using exit-code `1`             |
+
+```bash
+# Navigate into the `smarthome-go/smarthome` repository
+# cd smarthome
+
+# Run following command AFTER the image has been built
+# This will abort the integration tests
+# due to the `xxx_SETUP_ONLY` environment variable bein set to `1`
+docker run -it \
+  --rm \
+  --name smarthome-server-integration-tests \
+  -e 'SMARTHOME_INTEGRATION_SETUP_ONLY'=1 \
   -v $(pwd):/opt/smarthome/tests \
   mikmuellerdev/smarthome-integration-test
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ setup_mariadb () {
     cd '/usr' || exit 1
     /usr/bin/mysqld_safe --datadir='/var/lib/mysql' &
 
-    # Wait until the datbase is online
-    # Use a cheap query to check if the datbase is online
+    # Wait until the database is online
+    # Use a cheap query to check if the database is online
     while ! mysql -u root -password='' -e 'DO 1;'
     do
         echo "Waiting for database..."
@@ -24,15 +24,17 @@ setup_mariadb () {
 EOF
 }
 
-# Runs the tests
-run_integration_tests () {
-    # Test if the directory is valid
+# Test if the directory is valid (contains a go.mod file)
+check_go_directory() {
     cd /opt/smarthome/tests || exit 1
     if [ ! -f go.mod ]; then
         echo "You can only run integration tests inside a Go working directory"
         exit 1
     fi
+}
 
+# Runs the tests
+run_integration_tests () {
     # Run the actual tests
     echo "Running integration tests..."
     # Use local Makefile
@@ -42,4 +44,20 @@ run_integration_tests () {
 
 # START
 setup_mariadb
+check_go_directory
+
+# If `SMARTHOME_INTEGRATION_SETUP_ONLY` is set, do not run the integration tests
+if [[ -n "$SMARTHOME_INTEGRATION_SETUP_ONLY" ]]; then
+    if [ "$SMARTHOME_INTEGRATION_SETUP_ONLY" == "0" ]; then
+        echo "SMARTHOME_INTEGRATION_SETUP_ONLY is set to '0': continuing integration tests"
+    elif [ "$SMARTHOME_INTEGRATION_SETUP_ONLY" == '1' ]; then
+        echo "SMARTHOME_INTEGRATION_SETUP_ONLY is set to '1': omitting integration tests"
+        exit 0
+    else
+        echo "SMARTHOME_INTEGRATION_SETUP_ONLY is specified but holds invalid value: '$SMARTHOME_INTEGRATION_SETUP_ONLY': aborting integration tests"
+        exit 1
+    fi
+fi
+
+# Otherwise, run the integration tests
 run_integration_tests
